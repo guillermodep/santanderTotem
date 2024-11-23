d37129e261e1616ac9e8d54c6772c7c4021c4e8e
Guille Dep <58941444+guillermodep@users.noreply.github.com>
2024-11-22 21:50:41 -0500

    Update CardPrinting.tsx
    linea 194 agregada y 193 comentada

--- src/components/services/CardPrinting.tsx
+++ src/components/services/CardPrinting.tsx
@@ -190,7 +190,8 @@ export default function CardPrinting() {
                 setTimeout(() => setShowCardAnimation(false), 4000);
               }} className="transform transition-all duration-300">
                 <h3 className="text-xl font-bold text-center mb-4 flex items-center justify-center gap-2">
-                  <Star className="w-6 h-6 text-yellow-500" />
+//                  <Star className="w-6 h-6 text-yellow-500" />
+                  <CreditCard className="w-6 h-6 text-santander-red" />
                   Tarjeta de Crédito
                 </h3>
                 {renderCard('credit', true)}
